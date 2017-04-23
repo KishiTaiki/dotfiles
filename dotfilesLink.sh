e395d0fe9f7fe79b7b6810a23ea77e2b55c7f695
KishiTaiki <gansan-1230@ezweb.ne.jp>
2017-04-23 22:23:59 +0900

    initial commit

---
+++ dotfilesLink.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+ln -s ~/dotfiles/.Xmodmap  ~/.Xmodmap
+ln -s ~/dotfiles/.vim      ~/.vim
+ln -s ~/dotfiles/.vimrc    ~/.vimrc
+ln -s ~/dotfiles/.xinitrc  ~/.xinitrc
+ln -s ~/dotfiles/.xmonad   ~/.xmonad
+ln -s ~/dotfiles/.zshrc    ~/.zshrc
+
